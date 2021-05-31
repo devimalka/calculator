c6ec0f65b8bced335b5049d3ef7b9928aa8fa7f6
devim <developerimalka@gmail.com>
2021-05-31 22:24:10 +0530

    about to reset code

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function divide(a,b){
 }
 
 function operator(op,num1,num2){
-    let results;
+   
     switch(op){
         case '+':
           return add(num1,num2);
@@ -42,25 +42,94 @@ function operator(op,num1,num2){
 
     }
    
-
-
-
 }
 
 
+let hiddenvalue = document.getElementsByClassName("hiddenvalue");
+
+let score = "";
+
+
 function display(nums){
-     
+    
     let disp = document.getElementsByClassName('disp');
-    disp[0].innerHTML=nums;
+    disp[0].innerHTML+=nums;
 
 }
 
 
 
 function reply(clickedid){
-
-
+    if(clickedid=="+"|| clickedid=='/'|| clickedid=='*'||clickedid=='-' || clickedid == '='){
+        score = "";
+    }
+    else{
+    score+=clickedid;
+    console.log(score);
+    }
     display(clickedid);
        
 }
 
+
+function checkop(first,second){
+    if(second != null && first !=null){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+
+let firstoperand = null;
+let secondoperand = false;
+let sums;
+let operand;
+let second = null;
+let first=null;
+let secondo = false;
+
+function operatorHandle(op){
+    if(first==null && second==false){
+        first = op;
+    }
+    else if(first =!null && second == false){
+        secondo = op;
+    }
+
+}
+
+function sum(op)
+{
+    
+    let check = parseFloat(op);
+    if(isNaN(check)){
+
+        if(firstoperand == null && secondoperand == false){
+            firstoperand = parseFloat(score);
+            operatorHandle(op);
+        }
+        else if(firstoperand !=null && secondoperand == false){
+            secondoperand = parseFloat(score);
+            operatorHandle(op);
+           
+        }
+        else if(first !=null && secondoperand !=null){
+            if(first='+'){
+                sums = add(firstoperand,secondoperand);
+                console.log(sums);
+                first = secondo;
+                secondo = false;
+            }
+        }
+
+
+    }
+   
+       console.log(firstoperand,op,secondoperand);
+            
+}
+
+        
