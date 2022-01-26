c2b0bf3e65e29ec8b66a80f811ded1b5ca3c54bf
Oshada Kularathne <Oshada@kdaw.com>
2022-01-26 09:17:45 +0530

    basic add mul sub div functions

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function add(a,b){
+    return a+b;
+}
+
+
+function substract(a,b){
+    return a-b;
+
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+
+console.log(add(2,4));
+console.log(substract(2,4));
+console.log(multiply(2,4));
+console.log(divide(2,4));
