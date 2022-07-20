34c05cb898613664ab61809a2647f86ea220ae0d
devim <developerimalka@gmail.com>
2022-07-20 17:46:30 +0530

    new branch

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ function divide(a,b){
 }
 
 
+function operator(operator,a,b){
+    switch(operator){
+        case '-':
+            return substract(a,b);
+            break;
+        case '+':
+            return add(a,b);
+            break;
+        case '/':
+            return divide(a,b);
+            break;
+        case '*':
+            return multiply(a,b);
+            break;
+
+
+    }
+}
+
+
+
+const btns = document.getElementsByTagName("button")
+function printfunct(){
+    console.log("HI")
+}
+
+
+for(var i=0;i<btns.length;i++){
+    console.log(btns[i])
+    btns[i].addEventListener('click',console.log("test"));
+}
+
